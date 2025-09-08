b49fb5ea23e144a7fe7574c2849cd5786b0543a9
lugaye <160602222+lugaye@users.noreply.github.com>
2025-09-08 20:07:17 +0300

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const ball = document.getElementById('ball');
+
+function play(){
+    ball.style.animationPlayState = 'running';
+}
+
+function pause(){
+    ball.style.animationPlayState = 'paused';
+}
